7cee20f61188573e261b92c8f66ec5ad56106cff
konghayao <3446798488@qq.com>
2022-09-05 10:06:41 +0800

    style: 修改主页样式

--- src/poetry/LunYu/IndexPage.tsx
+++ src/poetry/LunYu/IndexPage.tsx
@@ -11,27 +11,35 @@ export const IndexPage: FC<{ data: FetchData }> = function ({ data }) {
             <div
                 className="box-col content-max"
                 style={{
-                    borderTop: "3px solid var(--divide-red)",
-                    borderBottom: "3px solid var(--divide-red)",
                     height: "100%",
                     flex: "1",
-                    margin: "1rem 0",
-                    padding: "2rem",
                 }}>
-                <div className="Index-Seal">论语</div>
-                <Grid.Row style={{ fontSize: "1.5rem" }} justify="start">
-                    {data.map((i) => {
-                        return (
-                            <Grid.Col span={4} key={"to-" + i.chapter}>
-                                <NavLink to={`/lunyu/${i.chapter}`}>
-                                    <Tag style={{ fontSize: "1.125rem" }}>
-                                        {i.chapter}
-                                    </Tag>
-                                </NavLink>
-                            </Grid.Col>
-                        );
-                    })}
-                </Grid.Row>
+                <div>
+                    <div className="Index-Seal">论语</div>
+                </div>
+                <div
+                    className="box-col"
+                    style={{
+                        flex: "1",
+                        justifyContent: "center",
+                        borderTop: "3px solid var(--divide-red)",
+                        borderBottom: "3px solid var(--divide-red)",
+                        margin: "2rem 0 ",
+                    }}>
+                    <Grid.Row style={{ fontSize: "1.5rem" }} justify="start">
+                        {data.map((i) => {
+                            return (
+                                <Grid.Col span={4} key={"to-" + i.chapter}>
+                                    <NavLink to={`/lunyu/${i.chapter}`}>
+                                        <Tag style={{ fontSize: "1.125rem" }}>
+                                            {i.chapter}
+                                        </Tag>
+                                    </NavLink>
+                                </Grid.Col>
+                            );
+                        })}
+                    </Grid.Row>
+                </div>
             </div>
         </div>
     );
