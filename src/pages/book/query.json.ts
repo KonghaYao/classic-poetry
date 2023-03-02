51e10527d85d84d88ba9e6cedeabf2b6f67cad84
江夏尧 <3446798488@qq.com>
2023-03-02 15:38:48 +0800

    refractor: 时候 MongoDB 后端

--- src/pages/book/query.json.ts
+++ src/pages/book/query.json.ts
@@ -1,6 +1,6 @@
 import type { APIRoute } from "astro";
-import fs from "fs";
-
+import mongoose from "mongoose";
+// import { _Poetry } from "../../model";
 export const get: APIRoute = async ({ request }) => {
     const params = Object.fromEntries([
         ...new URL(request.url).searchParams.entries(),
@@ -18,14 +18,26 @@ export const get: APIRoute = async ({ request }) => {
     //         import.meta.url
     //     )
     // );
-    const file = await fs.promises.readFile(
-        `./data/indexes/${params.name}.json`,
-        "utf-8"
-    );
-    const data: { id: string; title: string }[] = JSON.parse(file).slice(
-        (page - 1) * limit,
-        page * limit
-    );
+    // 上传至 mongoDB 主机, 注意，并不会跟随 Docker 打包
+    const data = await mongoose
+        .connect(
+            "mongodb+srv://reader:JqdKie3Zz7bKAxPz@cluster1.xavvdnu.mongodb.net/poetry?retryWrites=true&w=majority"
+        )
+        .then(async (db) => {
+            console.log("连接成功");
+            const data = await db.models.Poetry.find(
+                { belongToName: params.name },
+                { content: 0, notes: 0 }
+            )
+                .skip(page)
+                .limit(limit)
+                .lean(true)
+                .exec();
+            console.log("返回成功");
+            mongoose.disconnect();
+            return data;
+        });
+
     return {
         body: JSON.stringify({
             data,
