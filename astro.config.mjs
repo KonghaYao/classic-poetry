cbaf17d1c1eef18296cf3b5cbb27623543fac336
江夏尧 <3446798488@qq.com>
2023-08-27 09:35:16 +0800

    feat: 开始框架迁移

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify/functions";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind()],
+    integrations: [tailwind(), solidJs()],
     output: "server",
     adapter: netlify(),
+    // vite: {
+    //     resolve: {
+    //         alias: {
+    //             "@arco-design/web-react/icon":
+    //                 "@arco-design/web-react/icon/index.es.js",
+    //             "@arco-design/web-react": "@arco-design/web-react/es/index.js",
+    //         },
+    //     },
+    // },
 });
