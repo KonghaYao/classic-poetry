822947c6ed2749e72c86c5c8f3b56ece623d7dfe
江夏尧 <3446798488@qq.com>
2023-09-02 20:50:57 +0800

    feat: 添加分页加载

--- src/components/CatalogueList.tsx
+++ src/components/CatalogueList.tsx
@@ -1,42 +1,75 @@
 import { getIndex } from "../backend/getClient";
-import { Atom, atom, usePaginationStack } from "@cn-ui/reactive";
-export const useCatalogueListLoad = (name: string) =>
-    usePaginationStack(async (pageNumber: number, maxPage: Atom<number>) => {
-        return getIndex()
-            .search("", {
-                filter: `belongToName = '${name}'`,
-                limit: 30,
-                offset: pageNumber * 30,
-                attributesToRetrieve: ["author", "belongToName", "id", "title"],
-            })
-            .then((res) => res.hits)
-            .finally(() => maxPage((i) => i++));
-    }, {});
+import { Atom, atom, DebounceAtom, usePaginationStack } from "@cn-ui/reactive";
+import { Show } from "solid-js";
+export const useCatalogueListLoad = (name: string, searchText = atom("")) =>
+    usePaginationStack(
+        async (pageNumber: number, maxPage: Atom<number>) => {
+            return getIndex()
+                .search(searchText(), {
+                    filter: `belongToName = '${name}'`,
+                    limit: 30,
+                    offset: pageNumber * 30,
+                    attributesToRetrieve: [
+                        "author",
+                        "belongToName",
+                        "id",
+                        "title",
+                    ],
+                })
+                .then((res) => res.hits)
+                .finally(() => maxPage((i) => i++));
+        },
+        {
+            deps: [DebounceAtom(searchText, 1000)],
+        }
+    );
+
+/** 特别为 input 组件使用的事件绑定 */
+function AtomToModel<T>(a: Atom<T>) {
+    const oninput = (e: any) => {
+        a(() => e.target.value);
+    };
+    return { "on:input": oninput, value: a() };
+}
 export const CatalogueList = (props: { name: string }) => {
     const ref = atom<HTMLDivElement | null>(null);
-    const { dataSlices, currentData } = useCatalogueListLoad(props.name);
-    // const { data, error, loading } = useBookIndexMapper(props);
+    const searchText = atom("");
+    const { dataSlices, currentData, next } = useCatalogueListLoad(
+        props.name,
+        searchText
+    );
     return (
-        <section class="box-col no-scroll link-list" ref={ref}>
-            <ul class="flex flex-wrap justify-between text-2xl gap-2">
-                {(dataSlices() ?? []).flat().map((i) => {
-                    return (
-                        <a
-                            href={`/poetry/${i.id}`}
-                            class="text-lg bg-gray-100 dark:bg-gray-700 rounded-lg border-1 border-white p-2 text-[#496884] dark:text-[#a3b9cc]">
-                            <li class="long-list-item  hover:brightness-110">
-                                {i.title}
-                            </li>
-                        </a>
-                    );
-                })}
-                <div class="w-full flex justify-center items-center text-sm ">
-                    {currentData() &&
-                        currentData().length === 0 &&
-                        "没有更多啦"}
-                    {currentData.loading() && "加载中"}
-                </div>
-            </ul>
-        </section>
+        <>
+            <input type="text" {...AtomToModel(searchText)} />
+            <section class="flex flex-col no-scroll link-list" ref={ref}>
+                <ul class="flex flex-wrap justify-between text-2xl gap-2">
+                    {(dataSlices() ?? []).flat().map((i) => {
+                        return (
+                            <a
+                                href={`/poetry/${i.id}`}
+                                class="text-lg bg-gray-100 dark:bg-gray-700 rounded-lg border-1 border-white p-2 text-[#496884] dark:text-[#a3b9cc]">
+                                <li class="long-list-item  hover:brightness-110">
+                                    {i.title}
+                                </li>
+                            </a>
+                        );
+                    })}
+                    <Show
+                        when={currentData()?.length}
+                        fallback={
+                            <span class="text-lg bg-gray-100 dark:bg-gray-700 rounded-lg border-1 border-white p-2 text-yellow-500 flex-1 cursor-pointer text-center">
+                                {currentData()?.length === 0 && "没有更多啦"}
+                                {currentData.loading() && "加载中"}
+                            </span>
+                        }>
+                        <span
+                            class="text-lg bg-gray-100 dark:bg-gray-700 rounded-lg border-1 border-white p-2 text-yellow-500 flex-1 cursor-pointer text-center"
+                            onclick={next}>
+                            加载更多
+                        </span>
+                    </Show>
+                </ul>
+            </section>
+        </>
     );
 };
