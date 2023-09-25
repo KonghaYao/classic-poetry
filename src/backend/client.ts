1a17edec0c6f4ad37b2b06ef77e00fa2ef71a236
江夏尧 <3446798488@qq.com>
2023-09-25 20:32:06 +0800

    fix: 修复trpc错误

--- src/backend/client.ts
+++ src/backend/client.ts
@@ -1,6 +1,6 @@
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
 import type { AppRouter } from './main';
-const origin = globalThis.document ? 'https://chinese-poetry.netlify.app' : "http://localhost:4321"
+const origin = globalThis.document ? '' : import.meta.env.PUBLIC_TRPC_URL
 export const client = createTRPCProxyClient<AppRouter>({
     links: [
         httpBatchLink({
