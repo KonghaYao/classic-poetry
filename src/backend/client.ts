0c3085a68ae226b6814c2b9f63df80f8d446759a
江夏尧 <3446798488@qq.com>
2023-09-25 19:56:41 +0800

    fix: 尝试修复 netlify 部署问题

--- src/backend/client.ts
+++ src/backend/client.ts
@@ -1,6 +1,6 @@
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
 import type { AppRouter } from './main';
-const origin = globalThis.document ? '' : "http://localhost:4321"
+const origin = globalThis.document ? 'https://chinese-poetry.netlify.app' : "http://localhost:4321"
 export const client = createTRPCProxyClient<AppRouter>({
     links: [
         httpBatchLink({
